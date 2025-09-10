fd170b9678ff2f6af3b7c57a3cabb8060512f7b1
logesh kumar <118279520+Logesh314@users.noreply.github.com>
2025-09-10 18:56:22 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,6 +25,10 @@ app.use("/api/message", messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT;
